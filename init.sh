dfc0248d6222813cc4e102ee096e96409f1f200b
TatianaVS <samsonovatv@gmail.com>
2016-03-07 23:46:58 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 mysql -uroot -e "CREATE DATABASE qa"
+mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';
+					GRANT ALL ON qa.* TO 'qauser'@'localhost';"
 
 
 sudo python /home/box/web/ask/manage.py syncbd
