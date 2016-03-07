f65c3787ac86b6f929c4a56985c2b9a10b006452
TatianaVS <samsonovatv@gmail.com>
2016-03-07 22:36:38 +0300

    Update and rename gunicorn.conf to hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -2,11 +2,12 @@
 CONFIG = {
     'mode': 'wsgi',
     'working_dir': '/home/box/web',
+    'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=4',
+        '--workers=16',
         '--timeout=60',
-        'hello:app',
+		'--log-level=debug',
+        'hello',
     ),
 }
