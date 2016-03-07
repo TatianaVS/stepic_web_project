bff9af76ebab7e0a7fda55b3aa4b73696907ee8c
TatianaVS <samsonovatv@gmail.com>
2016-03-07 22:43:21 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse 
+#from django.shortcuts import render
 
+from django.http import HttpResponse 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
-
-def qid(request, q_id, *args, **kwargs):
-    return HttpResponse("You're looking at question %s." % q_id)
