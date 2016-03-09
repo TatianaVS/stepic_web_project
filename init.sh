6d2ad56797d4a6c653dd6130c7b00e0147d40600
TatianaVS <samsonovatv@gmail.com>
2016-03-09 16:44:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,6 @@
-mysql -uroot -e "CREATE DATABASE qa"
-mysql -uroot -e "CREATE USER 'qauser'@'localhost' IDENTIFIED BY 'qapass';
-					GRANT ALL ON qa.* TO 'qauser'@'localhost';"
+if [ -e /etc/nginx/sites-enabled/default ] ; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
 
-
-sudo python /home/box/web/ask/manage.py syncdb
-
-sudo rm -r /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
-sudo /etc/init.d/gunicorn restart
