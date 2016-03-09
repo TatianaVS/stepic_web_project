9b2b05c9f2512130f3ef3e1231a3c17ddf63421e
TatianaVS <samsonovatv@gmail.com>
2016-03-09 17:08:24 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,16 @@
-from django.conf.urls import patterns, include, url
+from .views import test
+from django.conf.urls import url
 
-urlpatterns = patterns('qa.views',
-	url(r'^$', 'test', name='index'),
-	url(r'^login/', 'test', name='login'),
-	url(r'^signup/', 'test', name='signup'),
-	url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
-	url(r'^ask/', 'test', name='ask'),
-	url(r'^popular/', 'test', name='popular'),	
-	url(r'^new/', 'test', name='new'),	
-)
+
+urlpatterns = [
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/([0-9]+)/$', test),
+    url(r'^ask/\w*/ $', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
+     url(r'^popular/', list_popular, name='list-popular'),
+     url(r'^answer/', post_answer, name='post_answer'),
+     url(r'^question/(?P<slug>\w+)/$', show_question, name='show-question'),
+]
