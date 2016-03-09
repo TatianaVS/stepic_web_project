39421d12e416433d39d0645a61ea39ed26c1ea6d
TatianaVS <samsonovatv@gmail.com>
2016-03-09 16:45:27 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python
-from cgi import parse_qs                                                        
-
-def application(environ, start_response):
-
-	queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-	body = ''
-	print(queryString)
-	for key, value in queryString.items() :
-		for element in value :
-			body += key + '='                                       
-			body += element + '\r\n'
-
-	print(body)                                                             
-		
-	status = '200 OK'
-
-	response_headers = [
-		('Content-Type', 'text/plain'),
-	]
-	start_response(status, response_headers)                                    
-
-	return [body]
+def app(env, start_response):
+  status = '200 OK'
+  headers = [('Content-Type', 'text/plain')]
+  body = env['QUERY_STRING'].replace('&', '\n')
+  start_response(status, headers)
+  return [body]
