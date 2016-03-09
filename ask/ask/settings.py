c21e98e374c8a94bf7cf4f482ea2d9f2c4a62622
TatianaVS <samsonovatv@gmail.com>
2016-03-09 13:09:22 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE' : 'django,.db.backends.mysql',
+        'ENGINE' : 'django,.db.backends.sqlite3',
         'NAME' : 'qa',
 		'DATABASE_HOST' : '/var/run/mysql',
 		'USER' : 'qauser',
