2d1e18b4fd15452d1174ccff929c119cf309ec5f
TatianaVS <samsonovatv@gmail.com>
2016-03-09 17:07:35 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,16 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
+from django.contrib.auth.models import User
+
 # Create your models here.
+# вопрос
+class Question(models.Model):
+    # заголовок вопроса
+    title = models.CharField(max_length=255)
+    # полный текст вопроса
+    text = models.TextField()
+    # дата добавления вопроса
+    added_at = models.DateTimeField(blank=True, null=True)
+    #рейтинг вопроса (число)
+    rating = models.IntegerField(default = 0)
+    #автор вопроса
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    #список пользователей, поставивших "лайк"
+    likes = models.ManyToManyField(User, related_name = '+')
+    def __unicode__(self):
+        return self.title
 
-class Question(models.Model) :
-	title = models.CharField(max_length=255)
-	text = models.TextField()
-	added_at = models.DateTimeField(blank=True)
-	rating = models.IntegerField()
-	author = models.ForeignKey(User, related_name='+') 
-	likes = models.ManyToManyField(User)
+# ответ
+class Answer(models.Model):
+    # текст ответа
+    text = models.TextField()
+    # дата добавления ответа
+    added_at = models.DateTimeField(blank=True, null=True)
+    # вопрос, к которому относится ответ
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    #автор ответа
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    def __unicode__(self):
+        return self.text
 
-class Answer(models.Model) :
-	text = models.TextField()
-	added_at = models.DateTimeField(blank=True)
-	question = models.ForeignKey(Question, related_name='+')
-	author = models.ForeignKey(User, related_name='+')
